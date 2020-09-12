773d0cfd140aaa43783932537510b2bebef011cb
Luis Filipe Alves de Oliveira <macbook-pro-lf-oliveira@MacBook-Pro-de-Luis.local>
2020-09-11 21:46:07 -0300

    add servic e

--- backend/src/routes/appointments.router.ts
+++ backend/src/routes/appointments.router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { startOfHour, parseISO } from "date-fns";
+import { parseISO } from "date-fns";
 
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import CreateAppointmentService from "../services/CreateAppointmentService";
 
 const appointmentsRouter = Router();
 
@@ -14,27 +15,26 @@ appointmentsRouter.get("/", (request, response) => {
 });
 
 appointmentsRouter.post("/", (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parsedDate
-  );
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository
+    );
 
-  if (findAppointmentInSameDate) {
-    return response.status(400).json({
-      message: "This appointment alerady book",
-      module: "Appointment",
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
     });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response
+      .status(400)
+      .json({ error: err.message, module: "appointment" });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
